9a01e8f69e88cbc6aed306d158479f34dc2edfba
Igor Kromin <igor@igorkromin.net>
2016-03-24 12:00:12 +1000

    Typo correction

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xms128m -Xmx128m -classpth lib/slf4j-api-1.7.2.jar:lib/yahoo-weather-java-api-1.2.0.jar -jar out/artifacts/jphotoframe/jphotoframe.jar&
+java -Xms128m -Xmx128m -classpath lib/slf4j-api-1.7.2.jar:lib/yahoo-weather-java-api-1.2.0.jar -jar out/artifacts/jphotoframe/jphotoframe.jar&
