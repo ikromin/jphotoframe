b1626ec8120605ad14fea6228bcb01d75814cc82
Igor Kromin <igor@igorkromin.net>
2016-03-24 11:55:35 +1000

    Run script update

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-cd /jphotoframe
-java -Xms32m -Xmx32m -jar out/artifacts/jphotoframe/jphotoframe.jar&
+java -Xms128m -Xmx128m -classpth lib/slf4j-api-1.7.2.jar:lib/yahoo-weather-java-api-1.2.0.jar -jar out/artifacts/jphotoframe/jphotoframe.jar&
