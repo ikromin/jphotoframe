e646f50e886b660c75d714ee8030aa614a220dad
Igor Kromin <igor@igorkromin.net>
2017-11-07 22:38:22 +1100

    Added missing license info

--- src/main/java/net/igorkromin/jphotoframe/ui/widgets/WeatherForecast.java
+++ src/main/java/net/igorkromin/jphotoframe/ui/widgets/WeatherForecast.java
@@ -1,3 +1,23 @@
+/**
+ * JPhotoFrame - a simple Java application for displaying a collection of photos in a full-screen slideshow.
+ * Copyright (C) 2015  Igor Kromin
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * You can find this and my other open source projects here - http://github.com/ikromin
+ */
+
 package net.igorkromin.jphotoframe.ui.widgets;
 
 import net.igorkromin.jphotoframe.ui.ModelData;
